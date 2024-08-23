d6cd5b4faa35f0a52dfc6f2b521e5c8ffd06efab
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-08-23 09:44:10 +0500

    Bug Fix

--- app/src/main/java/com/example/tiktokvideo_downloader/domain/model/tiktok/TikTok.kt
+++ app/src/main/java/com/example/tiktokvideo_downloader/domain/model/tiktok/TikTok.kt
@@ -1,17 +1,18 @@
 package com.example.tiktokvideo_downloader.domain.model.tiktok
 
 
+import androidx.annotation.Nullable
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TikTok(
     @SerialName("code")
-    val code: Int,
+    val code: Int? = null,
     @SerialName("data")
-    val `data`: Data,
+    val `data`: Data? = null,
     @SerialName("msg")
-    val msg: String,
+    val msg: String? = null,
     @SerialName("processed_time")
-    val processedTime: Double
+    val processedTime: Double? = null
 )
--- app/src/main/java/com/example/tiktokvideo_downloader/utlis/Constant.kt
+++ app/src/main/java/com/example/tiktokvideo_downloader/utlis/Constant.kt
@@ -2,6 +2,6 @@ package com.example.tiktokvideo_downloader.utlis
 
 object Constant {
     const val API_HOST = "tiktok-download-without-watermark.p.rapidapi.com"
-    const val API_KEY = "960edc122fmsh32d9e50d4d01ae2p14d535jsn88155bc7bbf7"
+    const val API_KEY = "0e3e36a41dmsh01f5d1b030cc6cfp103c0ejsn9c29801473d0"
     const val TIMEOUT = 15_000L
 }
